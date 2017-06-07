2128ef87c00725801e9aab103f8c2f48a3754a4b
DevonYarbrough <devonyarbrough@icloud.com>
2017-06-06 21:10:33 -0400

    creating unity project and animations

---
+++ RPG_2d/Assets/scripts/cameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	public Transform target;
+	Camera myCamera;
+	// Use this for initialization
+	void Start () {
+		myCamera = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		myCamera.orthographicSize = (Screen.height / 60f);
+
+		if (target) {
+			transform.position = Vector3.Lerp (transform.position, target.position, 0.2f) + new Vector3(0, 0, -10);
+		}
+
+	}
+}
